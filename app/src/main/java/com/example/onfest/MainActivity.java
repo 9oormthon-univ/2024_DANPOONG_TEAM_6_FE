4f3bd16dd19240e0e43a2d0a559801f741368c92
ji <wldbs0898@naver.com>
2024-11-24 01:47:14 +0900

    feat: kakaoLogin 추가

--- app/src/main/java/com/example/onfest/MainActivity.java
+++ app/src/main/java/com/example/onfest/MainActivity.java
@@ -1,6 +1,16 @@
 package com.example.onfest;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +18,156 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.kakao.sdk.auth.model.OAuthToken;
+import com.kakao.sdk.user.UserApiClient;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import kotlin.Unit;
+import kotlin.jvm.functions.Function2;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.Body;
+import retrofit2.http.Header;
+import retrofit2.http.POST;
+
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "KakaoLogin";
+    private View loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        getHashKey();
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        SharedPreferences sharedPreferences = getSharedPreferences("AuthPrefs", Context.MODE_PRIVATE);
+        String accessToken = sharedPreferences.getString("AccessToken", null);
+
+
+        loginButton = findViewById(R.id.login);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        // 카카오톡이 설치되어 있는지 확인하는 메서드 , 카카오에서 제공함. 콜백 객체를 이용합.
+        Function2<OAuthToken, Throwable, Unit> callback = new Function2<OAuthToken, Throwable, Unit>() {
+            @Override
+            // 콜백 메서드 ,
+            public Unit invoke(OAuthToken oAuthToken, Throwable throwable) {
+                Log.e(TAG, "CallBack Method");
+                //oAuthToken != null 이라면 로그인 성공
+                if (oAuthToken != null) {
+                    // 토큰이 전달된다면 로그인이 성공한 것이고 토큰이 전달되지 않으면 로그인 실패한다.
+                    //updateKakaoLoginUi();
+                    //ReservationData reservationData = new ReservationData(cake_image, year, month, day, hour, min);
+                    SharedPreferences sharedPreferences = getSharedPreferences("AuthPrefs", Context.MODE_PRIVATE);
+                    sharedPreferences.edit()
+                            .putString("AccessToken", oAuthToken.getAccessToken())
+                            .apply();
+
+                    String accessToken = oAuthToken.getAccessToken();
+                    Retrofit retrofit = RetrofitClient.getClient(accessToken);
+                    LoginApiService apiService = retrofit.create(LoginApiService.class);
+                    Call<Void> call = apiService.login("Bearer " + accessToken);
+                    call.enqueue(new Callback<Void>() {
+                        @Override
+                        public void onResponse(Call<Void> call, Response<Void> response) {
+                            Toast.makeText(MainActivity.this, "code: " + response.code(), Toast.LENGTH_LONG).show();
+                            if (response.isSuccessful()) {
+                                // 성공 처리
+                                Toast.makeText(MainActivity.this, "Login successful!", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(MainActivity.this, MakeProfile.class);
+                                startActivity(intent);
+                            } else {
+                                // 실패 처리
+                                Toast.makeText(MainActivity.this, "데이터 전송 및 예약에 실패했습니다, 다시 시도해주세요.", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(Call<Void> call, Throwable t) {
+                            // 오류 처리
+                            Log.d(TAG, t.getMessage());
+                            t.printStackTrace();
+                            Toast.makeText(MainActivity.this, "데이터 전송 중 오류가 발생하였습니다, 다시 시도해주세요.", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+                } else {
+                    //로그인 실패
+                    Log.e(TAG, "invoke: login fail");
+                }
+
+                return null;
+            }
+        };
+
+        // 로그인 버튼 클릭 리스너
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // 해당 기기에 카카오톡이 설치되어 있는 확인
+                if (UserApiClient.getInstance().isKakaoTalkLoginAvailable(MainActivity.this)) {
+                    UserApiClient.getInstance().loginWithKakaoTalk(MainActivity.this, callback);
+                } else {
+                    // 카카오톡이 설치되어 있지 않다면
+                    UserApiClient.getInstance().loginWithKakaoAccount(MainActivity.this, callback);
+                }
+            }
+        });
+
+//        Function2<OAuthToken,Throwable, Unit> callback =new Function2<OAuthToken, Throwable, Unit>() {
+//            @Override
+//            // 콜백 메서드 ,
+//            public Unit invoke(OAuthToken oAuthToken, Throwable throwable) {
+//                Log.e(TAG,"CallBack Method");
+//                //oAuthToken != null 이라면 로그인 성공
+//                if(oAuthToken!=null){
+//                    // 토큰이 전달된다면 로그인이 성공한 것이고 토큰이 전달되지 않으면 로그인 실패한다.
+//
+//                }else {
+//                    //로그인 실패
+//                    Log.e(TAG, "invoke: login fail" );
+//                }
+//
+//                return null;
+//            }
+//        };
+    }
+
+    private void getHashKey() {
+        PackageInfo packageInfo = null;
+        try {
+            packageInfo = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        if (packageInfo == null)
+            Log.e("KeyHash", "KeyHash:null");
+
+        for (Signature signature : packageInfo.signatures) {
+            try {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            } catch (NoSuchAlgorithmException e) {
+                Log.e("KeyHash", "Unable to get MessageDigest. signature=" + signature, e);
+            }
+        }
+    }
+
+    public static interface LoginApiService {
+        @POST("auth/kakao")
+        Call<Void> login(@Header("Authorization") String authHeader);
     }
 }
+
+////
